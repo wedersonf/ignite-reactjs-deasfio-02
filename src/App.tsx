7de26c2ca0a5b47305f8315c3675c2a2245b76d4
wedersonf <wedymf@hotmail.com>
2023-07-02 23:59:56 -0300

    feat: list header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { Header } from "./components/Header"
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className={styles.wrapper}>
+        <header className={styles.header}>
+          <div className={styles.headerWrapper}>
+            <strong>Tarefas criadas</strong>
+            <span>5</span>
+          </div>
+
+          <div className={styles.headerWrapper}>
+            <strong>Concluídas</strong>
+            <span>2 de 5</span>
+          </div>
+        </header>
+      </div>
     </>
   )
 }
